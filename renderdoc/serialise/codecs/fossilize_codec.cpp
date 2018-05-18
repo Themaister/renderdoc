7dbdffed4c3804bbfae794c395ee9737d03821e5
Hans-Kristian Arntzen <maister@archlinux.us>
2018-05-18 14:18:55 +0200

    Serialize descriptor set layouts and pipeline layouts.

--- renderdoc/serialise/codecs/fossilize_codec.cpp
+++ renderdoc/serialise/codecs/fossilize_codec.cpp
@@ -34,6 +34,7 @@ using namespace Fossilize;
 #define GET_F32() float((*args++)->data.basic.d)
 #define GET_ENUM(T) static_cast<T>((*args++)->data.basic.u)
 #define GET_HANDLE(t) (T)((*args++)->data.basic.u)
+#define GET_ARRAY() (*args++)->data.children.data()
 
 static bool serialise_sampler(StateRecorder &recorder, const SDObject *create_info, const SDObject *id)
 {
@@ -65,6 +66,108 @@ static bool serialise_sampler(StateRecorder &recorder, const SDObject *create_in
 	return true;
 }
 
+static void serialise_descriptor_set_binding(VkDescriptorSetLayoutBinding &binding,
+                                             const SDObject *arg)
+{
+	const SDObject * const *args = arg->data.children.data();
+	binding.binding = GET_U32();
+	binding.descriptorType = GET_ENUM(VkDescriptorType);
+	binding.descriptorCount = GET_U32();
+	binding.stageFlags = GET_U32();
+
+	const SDObject *immutable = *args;
+	vector<VkSampler> immutable_samplers(binding.descriptorCount);
+	if (!immutable->data.children.empty())
+	{
+		binding.pImmutableSamplers = immutable_samplers.data();
+		const SDObject * const *handles = immutable->data.children.data();
+		for (uint32_t i = 0; i < binding.descriptorCount; i++)
+			immutable_samplers[i] = (VkSampler)handles[i]->data.basic.u;
+	}
+	else
+		binding.pImmutableSamplers = NULL;
+}
+
+static bool serialise_descriptor_set_bindings(VkDescriptorSetLayoutBinding *binding, size_t count,
+                                              const SDObject *arg)
+{
+	const SDObject * const *args = arg->data.children.data();
+	for (size_t i = 0; i < count; i++)
+		serialise_descriptor_set_binding(binding[i], *args++);
+
+	return true;
+}
+
+static bool serialise_descriptor_set_layout(StateRecorder &recorder, const SDObject *create_info,
+                                            const SDObject *id)
+{
+	const SDObject * const *args = create_info->data.children.data();
+	VkDescriptorSetLayoutCreateInfo info = {};
+
+	info.sType = GET_ENUM(VkStructureType);
+	if ((*args++)->type.basetype != SDBasic::Null)
+		return false;
+	info.flags = GET_U32();
+	info.bindingCount = GET_U32();
+
+	vector<VkDescriptorSetLayoutBinding> bindings(info.bindingCount);
+	if (info.bindingCount)
+	{
+		info.pBindings = bindings.data();
+		serialise_descriptor_set_bindings(bindings.data(), bindings.size(), *args++);
+	}
+
+	unsigned index = recorder.register_descriptor_set_layout(
+			Hashing::compute_hash_descriptor_set_layout(recorder, info), info);
+	recorder.set_descriptor_set_layout_handle(index, (VkDescriptorSetLayout)id->data.basic.u);
+	return true;
+}
+
+static bool serialise_pipeline_layout(StateRecorder &recorder, const SDObject *create_info,
+                                      const SDObject *id)
+{
+	const SDObject * const *args = create_info->data.children.data();
+	VkPipelineLayoutCreateInfo info = {};
+
+	info.sType = GET_ENUM(VkStructureType);
+	if ((*args++)->type.basetype != SDBasic::Null)
+		return false;
+	info.flags = GET_U32();
+	info.setLayoutCount = GET_U32();
+
+	vector<VkDescriptorSetLayout> layouts(info.setLayoutCount);
+	const SDObject * const *set_layouts = GET_ARRAY();
+	if (info.setLayoutCount)
+	{
+		info.pSetLayouts = layouts.data();
+		for (uint32_t i = 0; i < info.setLayoutCount; i++)
+			layouts[i] = (VkDescriptorSetLayout)set_layouts[i]->data.basic.u;
+	}
+	else
+		info.pSetLayouts = NULL;
+
+	info.pushConstantRangeCount = GET_U32();
+	const SDObject * const *ranges = GET_ARRAY();
+	vector<VkPushConstantRange> push_ranges(info.pushConstantRangeCount);
+	if (info.pushConstantRangeCount)
+	{
+		info.pPushConstantRanges = push_ranges.data();
+		for (uint32_t i = 0; i < info.pushConstantRangeCount; i++)
+		{
+			VkPushConstantRange &r = push_ranges[i];
+			const SDObject * const *args = ranges[i]->data.children.data();
+			r.stageFlags = GET_U32();
+			r.offset = GET_U32();
+			r.size = GET_U32();
+		}
+	}
+
+	unsigned index = recorder.register_pipeline_layout(
+			Hashing::compute_hash_pipeline_layout(recorder, info), info);
+	recorder.set_pipeline_layout_handle(index, (VkPipelineLayout)id->data.basic.u);
+	return true;
+}
+
 ReplayStatus export_fossilize(const char *filename, const RDCFile &rdc, const SDFile &structData,
                               RENDERDOC_ProgressCallback progress)
 {
@@ -81,7 +184,17 @@ ReplayStatus export_fossilize(const char *filename, const RDCFile &rdc, const SD
 		}
 		else if (chunk->name == "vkCreateDescriptorSetLayout")
 		{
-
+			if (!serialise_descriptor_set_layout(recorder,
+			                                     chunk->data.children[1],
+			                                     chunk->data.children[3]))
+				return ReplayStatus::APIIncompatibleVersion;
+		}
+		else if (chunk->name == "vkCreatePipelineLayout")
+		{
+			if (!serialise_pipeline_layout(recorder,
+			                               chunk->data.children[1],
+			                               chunk->data.children[3]))
+				return ReplayStatus::APIIncompatibleVersion;
 		}
 	}
 
